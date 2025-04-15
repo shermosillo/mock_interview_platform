0f27259a6079c0e474eb5efb854eb661b18018b6
Sam <shermosillo27@gmail.com>
2025-04-14 19:54:50 -0700

    add auth page

---
+++ components/FormField.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function FormField = () => (
+    <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+);
+    
+export default FormField
