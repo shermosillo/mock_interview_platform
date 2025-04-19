47afd0132d62aa46e8007b69e860c00fb964f2f2
Sam <shermosillo27@gmail.com>
2025-04-19 11:40:26 -0700

    changes to the Authform

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,41 +1,52 @@
-"use client"
+"use client";
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import {Form} from "@/components/ui/form"
 import Link from "next/link"
+import { toast } from "sonner"
+import Image from "next/image"
+import FormField from "./FormField";
+import { useRouter } from "next/navigation";
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-})
 
+ const authFormSchema = (type: FormType) => {
+  return z.object({
+    name: type === 'sign-up' ? z.string().min(3) : z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(3),
+  })
+ }
 
-const AuthForm = ({ type } : { type: FormType }) => {
-// 1. Define your form.
+ const AuthForm = ({ type } : { type: FormType }) => {
+  const router = useRouter()
+  const formSchema = authFormSchema(type);
+ 
 const form = useForm<z.infer<typeof formSchema>>({
   resolver: zodResolver(formSchema),
   defaultValues: {
-    username: "",
+    name: "",
+    email: "",
+    password: "",
   },
 })
 
-// 2. Define a submit handler.
 function onSubmit(values: z.infer<typeof formSchema>) {
-  // Do something with the form values.
-  // ✅ This will be type-safe and validated.
-  console.log(values)
+  try {
+      if (type === 'sign-up') {
+        toast. success('Account created successfully. Please sign in.');
+          router.push('/sign-in');
+      }
+      else{
+        toast. success('Signed in successfully.');
+          router.push('/');
+      }
+  } catch (error) {
+    console.log(error);
+    toast.error(`There was an error: ${error}`)
+  }
 }
 
   const isSignIn = type === 'sign-in';
@@ -44,21 +55,34 @@ function onSubmit(values: z.infer<typeof formSchema>) {
     <div className="card-border lg:min-w-[566px]">
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
-          <img src="/logo.svg" alt="logo" height={32} width={38} />
+          <Image src="/logo.svg" alt="logo" height={32} width={38} />
           <h2 className="text-primary-100">PrepView</h2>
         </div>
         <h3>Practice Job Interview with AI</h3>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
-            {!isSignIn && <p>Name</p>}
-            <p>Email</p>
-            <p>Password</p>
+            {!isSignIn && (
+              <FormField control={form.control}
+                name="name"
+                label="Name"
+                placeholder="Your Name" />
+            )}
+            <FormField control={form.control}
+                name="email"
+                label="Email"
+                placeholder="Your Email Address"
+                type="email" />
+            <FormField control={form.control}
+                name="password"
+                label="Password"
+                placeholder="Enter Your Password"
+                type="password" />
             <Button className="btn" type="submit">{isSignIn ? 'Sign In' : 'Create an Account'}</Button>
           </form>
         </Form>
         <p className="text-center">
           {isSignIn ? 'No account yet?' : 'Have an account already?'}
-          <Link href={!isSignIn ? '/sign-in' : '/sign-out'} className="font-bold text-user-primary ml-1">
+          <Link href={!isSignIn ? '/sign-in' : '/sign-up'} className="font-bold text-user-primary ml-1">
             {!isSignIn ? 'Sign In' : 'Sign Up'}
           </Link>
         </p>
--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,22 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { FormControl, FormDescription, FormItem, FormLabel, FormMessage } from './ui/form';
+import { Input } from './ui/input';
+import { Control, Controller, FieldValues, Path } from 'react-hook-form';
 
-function FormField = () => (
-    <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: 'text' | 'email' | 'password' | 'file'
+}
+
+const FormField = ( { control, name, label, placeholder, type="text" } : FormFieldProps<T>) => (
+  <Controller 
+    name={name}
+    control={control}
+    render={({ field }) => (
+      <FormItem>
+          <FormLabel className="label">{label}</FormLabel>
+          <FormControl>
+            <Input
+            className="input"
+            placeholder={placeholder}
+            type={type}
+            {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+    )}
+  />
 );
     
 export default FormField
